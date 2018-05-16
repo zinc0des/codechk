e00555f0d7f319ea88b5647558523c074ed7aa60
Satavisha Roy <satavisha.roy@gmail.com>
2018-05-16 09:53:37 -0400

    all scripts till 051618

---
+++ src/codechk/codechk.java
@@ -0,0 +1,78 @@
+package codechk;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class codechk {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+//		switch(letter) {
+//	    System.out.println("switch reached");
+//	    case "A":
+//	        System.out.println("A reached");
+//	        break;
+//	    case "B":
+//	        System.out.println("B reached");
+//	        break;
+//	    default:
+//	        System.out.println("default reached");
+//	        break;
+//	}
+//		int count = 1;
+//		while (count < 11) {
+//			System.out.println("The count is " + count);
+//			count++; // remember, this increases the value of count by 1
+//		
+////}
+//		Scanner input = new Scanner(System.in);
+//		int[] pin = new int[4];
+//		int[] all = new int[4];
+//		//pin_input = 0;
+//		do {
+//			System.out.println("Please enter your PIN");
+//			for (int i = 0; i < pin.length; i++) {
+//				pin[i] = input.nextInt();
+//				System.out.print(pin[i] + " ");
+//				i++;
+//			}
+//			}while (true);
+//		/*new code block /*
+//		int value = 10;
+//		while (value >= 0) {
+//		    System.out.print(value + ", ");
+//		    value -=2;
+//		System.out.println("Please type a number");
+//		int num = input.nextInt();
+//		int count = 0;
+//		while (count <= num) {
+//			System.out.print(count + " ");
+//			count += 1;
+//			}
+//		System.out.println("\nI just counted to " + num + "!");
+		
+		/*new code block */
+//		 System.out.println("Enter a number:");
+//		 int num = input.nextInt();
+//		 int even = 2;
+//		 int count = 0;
+//		 while (even <= num) {
+//			 System.out.print(even + " ");
+//			 even += 2;
+//			 count += 1;
+//		 }
+//		 System.out.println("\nI just found " + count + " even numbers!");
+		
+	/*new code block*/
+String username;
+System.out.println("Enter a user name:");
+username = input.nextLine();
+
+	}
+
+  }
+	
+		
+	
+	
